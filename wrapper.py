eab83051f2caf5c311e101a32ad609f67be23324
zhaoleo1111 <46672085+zhaoleo1111@users.noreply.github.com>
2026-09-22 11:45:55 +0800

    Comment out attention module imports
    Comment out unused attention imports for clarity.

--- wrapper.py
+++ wrapper.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 from einops import rearrange
 import copy
-from attention.CBAM import CBAMBlock
-from attention.SEAttention import SEAttention
-from attention.BAM import BAM
+# from attention.CBAM import CBAMBlock
+# from attention.SEAttention import SEAttention
+# from attention.BAM import BAM
 
 def get_attention( preds, temp=1):
         """ preds: Bs*C*W*H """
